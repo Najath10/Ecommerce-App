226b935e0c47d6c5d33901231f29eb06ebbde020
Najath Mohammed <najathabdulla@gmail.com>
2025-04-06 17:53:06 +0530

    Added frontend React app

---
+++ ecommerce-frontend/ecom-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Products from './components/Products/Products'
+function App() {
+  
+  
+  return (
+   <>
+   
+      <Products/>
+   </>
+  )
+}
+
+export default App
+ 
