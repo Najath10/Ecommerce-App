f92f4f682db42eafc6bdbd81103d5e28fd338260
Najath Mohammed <najathabdulla@gmail.com>
2025-04-06 20:44:37 +0530

    Added Redux to State Mangement

--- ecommerce-frontend/ecom-app/src/api/api.js
+++ ecommerce-frontend/ecom-app/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:`http://localhost:8090/api`,
+    baseURL:`${import.meta.env.VITE_BACK_END_URL}/api`,
 })
 
 export default api;
--- ecommerce-frontend/ecom-app/src/store/reducer/store.js
+++ ecommerce-frontend/ecom-app/src/store/reducer/store.js
@@ -1,9 +1,10 @@
 
 import {configureStore} from '@reduxjs/toolkit'
+import { ProductReducer } from './ProductReducer';
 
  const store =configureStore({
     reducer:{
-
+        products:ProductReducer
     },
     preloadedState:{},
 });
